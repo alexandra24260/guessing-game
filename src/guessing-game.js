6d7b4f0cce8179f10e3094ab93192de52b807a64
Alexandra Vecher <alex192004@inbox.ru>
2019-10-11 12:21:30 +0300

    added solution of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max;
+        this.min;
+        this.res;
+    }
 
     setRange(min, max) {
-
+       this.max = max;
+       this.min = min;
     }
 
     guess() {
-
+        return this.res = Math.ceil(this.max - (this.max - this.min)/2);
     }
 
     lower() {
-
+        if (this.res < this.max){
+            return this.max = this.res;
+        }
     }
 
     greater() {
-
+        if (this.res < this.max){
+          return this.min = this.res;
+        }
     }
 }
 
